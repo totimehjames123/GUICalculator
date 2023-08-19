aa9015478490d6dcb2b3e6c5ae8ca921187925cc
James Tetteh Totimeh <totimehjames123@gmail.com>
2023-08-19 01:25:27 +0000

    done with phase 1

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        timesButton = New Button()
+        divisionButton = New Button()
+        minusButton = New Button()
+        fourButton = New Button()
+        oneButton = New Button()
+        equaltoButton = New Button()
+        dotButton = New Button()
+        sixButton = New Button()
+        threeButton = New Button()
+        fiveButton = New Button()
+        twoButton = New Button()
+        nineButton = New Button()
+        eightButton = New Button()
+        plusButton = New Button()
+        zeroButton = New Button()
+        sevenButton = New Button()
+        openBracketButton = New Button()
+        inputField = New RichTextBox()
+        closeBracketButton = New Button()
+        factorialButton = New Button()
+        clearButton = New Button()
+        SuspendLayout()
+        ' 
+        ' timesButton
+        ' 
+        timesButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        timesButton.Location = New Point(255, 267)
+        timesButton.Name = "timesButton"
+        timesButton.Size = New Size(75, 69)
+        timesButton.TabIndex = 0
+        timesButton.Text = "X"
+        timesButton.UseVisualStyleBackColor = True
+        ' 
+        ' divisionButton
+        ' 
+        divisionButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        divisionButton.Location = New Point(255, 342)
+        divisionButton.Name = "divisionButton"
+        divisionButton.Size = New Size(75, 67)
+        divisionButton.TabIndex = 1
+        divisionButton.Text = "/"
+        divisionButton.UseVisualStyleBackColor = True
+        ' 
+        ' minusButton
+        ' 
+        minusButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        minusButton.Location = New Point(255, 187)
+        minusButton.Name = "minusButton"
+        minusButton.Size = New Size(75, 74)
+        minusButton.TabIndex = 2
+        minusButton.Text = "-"
+        minusButton.UseVisualStyleBackColor = True
+        ' 
+        ' fourButton
+        ' 
+        fourButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        fourButton.Location = New Point(174, 189)
+        fourButton.Name = "fourButton"
+        fourButton.Size = New Size(75, 72)
+        fourButton.TabIndex = 3
+        fourButton.Text = "4"
+        fourButton.UseVisualStyleBackColor = True
+        ' 
+        ' oneButton
+        ' 
+        oneButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        oneButton.Location = New Point(174, 267)
+        oneButton.Name = "oneButton"
+        oneButton.Size = New Size(75, 69)
+        oneButton.TabIndex = 4
+        oneButton.Text = "1"
+        oneButton.UseVisualStyleBackColor = True
+        ' 
+        ' equaltoButton
+        ' 
+        equaltoButton.FlatStyle = FlatStyle.System
+        equaltoButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        equaltoButton.Location = New Point(174, 342)
+        equaltoButton.Name = "equaltoButton"
+        equaltoButton.Size = New Size(75, 67)
+        equaltoButton.TabIndex = 5
+        equaltoButton.Text = "="
+        equaltoButton.UseVisualStyleBackColor = True
+        ' 
+        ' dotButton
+        ' 
+        dotButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        dotButton.Location = New Point(12, 342)
+        dotButton.Name = "dotButton"
+        dotButton.Size = New Size(75, 67)
+        dotButton.TabIndex = 6
+        dotButton.Text = "."
+        dotButton.UseVisualStyleBackColor = True
+        ' 
+        ' sixButton
+        ' 
+        sixButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        sixButton.Location = New Point(12, 191)
+        sixButton.Name = "sixButton"
+        sixButton.Size = New Size(75, 69)
+        sixButton.TabIndex = 7
+        sixButton.Text = "6"
+        sixButton.UseVisualStyleBackColor = True
+        ' 
+        ' threeButton
+        ' 
+        threeButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        threeButton.Location = New Point(12, 267)
+        threeButton.Name = "threeButton"
+        threeButton.Size = New Size(75, 69)
+        threeButton.TabIndex = 8
+        threeButton.Text = "3"
+        threeButton.UseVisualStyleBackColor = True
+        ' 
+        ' fiveButton
+        ' 
+        fiveButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        fiveButton.Location = New Point(93, 189)
+        fiveButton.Name = "fiveButton"
+        fiveButton.Size = New Size(75, 72)
+        fiveButton.TabIndex = 9
+        fiveButton.Text = "5"
+        fiveButton.UseVisualStyleBackColor = True
+        ' 
+        ' twoButton
+        ' 
+        twoButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        twoButton.Location = New Point(93, 267)
+        twoButton.Name = "twoButton"
+        twoButton.Size = New Size(75, 69)
+        twoButton.TabIndex = 10
+        twoButton.Text = "2"
+        twoButton.UseVisualStyleBackColor = True
+        ' 
+        ' nineButton
+        ' 
+        nineButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        nineButton.Location = New Point(12, 117)
+        nineButton.Name = "nineButton"
+        nineButton.Size = New Size(75, 66)
+        nineButton.TabIndex = 11
+        nineButton.Text = "9"
+        nineButton.UseVisualStyleBackColor = True
+        ' 
+        ' eightButton
+        ' 
+        eightButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        eightButton.Location = New Point(93, 117)
+        eightButton.Name = "eightButton"
+        eightButton.Size = New Size(75, 66)
+        eightButton.TabIndex = 12
+        eightButton.Text = "8"
+        eightButton.UseVisualStyleBackColor = True
+        ' 
+        ' plusButton
+        ' 
+        plusButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        plusButton.Location = New Point(255, 115)
+        plusButton.Name = "plusButton"
+        plusButton.Size = New Size(75, 66)
+        plusButton.TabIndex = 13
+        plusButton.Text = "+"
+        plusButton.UseVisualStyleBackColor = True
+        ' 
+        ' zeroButton
+        ' 
+        zeroButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        zeroButton.Location = New Point(93, 342)
+        zeroButton.Name = "zeroButton"
+        zeroButton.Size = New Size(75, 67)
+        zeroButton.TabIndex = 14
+        zeroButton.Text = "0"
+        zeroButton.UseVisualStyleBackColor = True
+        ' 
+        ' sevenButton
+        ' 
+        sevenButton.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        sevenButton.Location = New Point(174, 117)
+        sevenButton.Name = "sevenButton"
+        sevenButton.Size = New Size(75, 66)
+        sevenButton.TabIndex = 15
+        sevenButton.Text = "7"
+        sevenButton.UseVisualStyleBackColor = True
+        ' 
+        ' openBracketButton
+        ' 
+        openBracketButton.Location = New Point(12, 82)
+        openBracketButton.Name = "openBracketButton"
+        openBracketButton.Size = New Size(75, 29)
+        openBracketButton.TabIndex = 18
+        openBracketButton.Text = ")"
+        openBracketButton.UseVisualStyleBackColor = True
+        ' 
+        ' inputField
+        ' 
+        inputField.AcceptsTab = True
+        inputField.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        inputField.Location = New Point(12, 12)
+        inputField.Multiline = False
+        inputField.Name = "inputField"
+        inputField.ReadOnly = True
+        inputField.RightToLeft = RightToLeft.No
+        inputField.ScrollBars = RichTextBoxScrollBars.ForcedVertical
+        inputField.Size = New Size(316, 45)
+        inputField.TabIndex = 19
+        inputField.Text = "0"
+        ' 
+        ' closeBracketButton
+        ' 
+        closeBracketButton.Location = New Point(93, 82)
+        closeBracketButton.Name = "closeBracketButton"
+        closeBracketButton.Size = New Size(75, 29)
+        closeBracketButton.TabIndex = 20
+        closeBracketButton.Text = "("
+        closeBracketButton.UseVisualStyleBackColor = True
+        ' 
+        ' factorialButton
+        ' 
+        factorialButton.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        factorialButton.Location = New Point(174, 82)
+        factorialButton.Name = "factorialButton"
+        factorialButton.Size = New Size(75, 29)
+        factorialButton.TabIndex = 21
+        factorialButton.Text = "!"
+        factorialButton.UseVisualStyleBackColor = True
+        ' 
+        ' clearButton
+        ' 
+        clearButton.Location = New Point(255, 82)
+        clearButton.Name = "clearButton"
+        clearButton.Size = New Size(75, 29)
+        clearButton.TabIndex = 22
+        clearButton.Text = "Clear"
+        clearButton.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(340, 416)
+        Controls.Add(clearButton)
+        Controls.Add(factorialButton)
+        Controls.Add(closeBracketButton)
+        Controls.Add(inputField)
+        Controls.Add(openBracketButton)
+        Controls.Add(sevenButton)
+        Controls.Add(zeroButton)
+        Controls.Add(plusButton)
+        Controls.Add(eightButton)
+        Controls.Add(nineButton)
+        Controls.Add(twoButton)
+        Controls.Add(fiveButton)
+        Controls.Add(threeButton)
+        Controls.Add(sixButton)
+        Controls.Add(dotButton)
+        Controls.Add(equaltoButton)
+        Controls.Add(oneButton)
+        Controls.Add(fourButton)
+        Controls.Add(minusButton)
+        Controls.Add(divisionButton)
+        Controls.Add(timesButton)
+        Name = "Form1"
+        RightToLeft = RightToLeft.Yes
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents timesButton As Button
+    Friend WithEvents divisionButton As Button
+    Friend WithEvents minusButton As Button
+    Friend WithEvents fourButton As Button
+    Friend WithEvents oneButton As Button
+    Friend WithEvents equaltoButton As Button
+    Friend WithEvents dotButton As Button
+    Friend WithEvents sixButton As Button
+    Friend WithEvents threeButton As Button
+    Friend WithEvents fiveButton As Button
+    Friend WithEvents twoButton As Button
+    Friend WithEvents nineButton As Button
+    Friend WithEvents eightButton As Button
+    Friend WithEvents plusButton As Button
+    Friend WithEvents zeroButton As Button
+    Friend WithEvents sevenButton As Button
+    Friend WithEvents openBracketButton As Button
+    Friend WithEvents inputField As RichTextBox
+    Friend WithEvents closeBracketButton As Button
+    Friend WithEvents factorialButton As Button
+    Friend WithEvents clearButton As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Numerics
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class Form1
+    Private currentCalculation As String = ""
+
+    Private Sub NumberButton_Click(sender As Object, e As EventArgs) Handles zeroButton.Click, oneButton.Click, twoButton.Click, threeButton.Click, fourButton.Click, fiveButton.Click, sixButton.Click, sevenButton.Click, eightButton.Click, nineButton.Click
+        Dim clickedButton As Button = CType(sender, Button)
+        currentCalculation += clickedButton.Text
+        inputField.Text = currentCalculation
+    End Sub
+
+    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles plusButton.Click, minusButton.Click, timesButton.Click, divisionButton.Click
+        Dim clickedButton As Button = CType(sender, Button)
+
+        If clickedButton.Text = "X" Then
+            currentCalculation += " " + "*" + " "
+            inputField.Text = currentCalculation
+        Else
+            currentCalculation += " " + clickedButton.Text + " "
+            inputField.Text = currentCalculation
+
+        End If
+
+    End Sub
+
+    Private Sub dotButton_Click(sender As Object, e As EventArgs) Handles dotButton.Click
+        If Not currentCalculation.EndsWith(".") Then
+            currentCalculation += "."
+            inputField.Text = currentCalculation
+        End If
+    End Sub
+
+    Private Sub openBracketButton_Click(sender As Object, e As EventArgs) Handles openBracketButton.Click
+        currentCalculation += "("
+        inputField.Text = currentCalculation
+    End Sub
+
+    Private Sub closeBracketButton_Click(sender As Object, e As EventArgs) Handles closeBracketButton.Click
+        currentCalculation += ")"
+        inputField.Text = currentCalculation
+    End Sub
+
+    Private Sub equaltoButton_Click(sender As Object, e As EventArgs) Handles equaltoButton.Click
+        Try
+            If IsValidExpression(currentCalculation) Then
+                Dim result As Double = EvaluateExpression(currentCalculation)
+                inputField.Text = result.ToString()
+                currentCalculation = result.ToString()
+            End If
+        Catch ex As Exception
+            inputField.Text = "Syntax error"
+        End Try
+
+
+    End Sub
+
+
+
+    Private Function EvaluateExpression(expression As String) As Double
+        Try
+            If IsValidExpression(currentCalculation) Then
+                Dim dt As New DataTable()
+                Dim result As Object = dt.Compute(expression, currentCalculation)
+                Return Convert.ToDouble(result)
+            End If
+        Catch ex As Exception
+            inputField.Text = "Syntax error!"
+        End Try
+
+
+    End Function
+
+    Private Sub clearButton_Click_1(sender As Object, e As EventArgs) Handles clearButton.Click
+        inputField.Clear()
+        currentCalculation = ""
+    End Sub
+
+    Private Function CalculateFactorial(number As BigInteger) As BigInteger
+        Try
+            Dim result As BigInteger = 1
+
+            For i As BigInteger = 2 To number
+                result *= i
+            Next
+
+            Return result
+        Catch ex As Exception
+            inputField.Text = "Stack error!"
+        End Try
+
+    End Function
+
+
+    Private Async Sub factorialButton_Click(sender As Object, e As EventArgs) Handles factorialButton.Click
+        Try
+            If BigInteger.TryParse(currentCalculation, Nothing) Then
+                Dim number As BigInteger = BigInteger.Parse(currentCalculation)
+
+                If number < 0 Then
+                    MessageBox.Show("Factorial is not defined for negative numbers.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return
+                End If
+
+                Dim factorialResult As BigInteger = Await Task.Run(Function() CalculateFactorial(number))
+                inputField.Text = factorialResult.ToString()
+                currentCalculation = factorialResult.ToString()
+            Else
+                MessageBox.Show("Invalid input for factorial calculation.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Function IsValidExpression(expression As String) As Boolean
+
+
+        If String.IsNullOrWhiteSpace(expression) Then
+            Return False
+        End If
+
+        Dim lastChar As Char = expression(expression.Length - 1)
+        If "+-*/.".Contains(lastChar) Then
+            Return False
+        End If
+
+        Dim openParenCount As Integer = expression.Count(Function(c) c = "(")
+        Dim closeParenCount As Integer = expression.Count(Function(c) c = ")")
+
+        If openParenCount <> closeParenCount Then
+            Return False
+        End If
+
+        Return True
+    End Function
+
+End Class
